0035ca616637b04109a1813f109348d0691da279
David Soards <david@soards.me>
2023-11-26 01:22:52 -0500

    use tokenize on client

--- src/stores/tokenize.ts
+++ src/stores/tokenize.ts
@@ -1,20 +1,13 @@
 import { ref } from 'vue';
+import { encode } from 'gpt-tokenizer';
 import { defineStore } from 'pinia';
-
-import $http from '@/utils/http';
 
 export const useTokenizeStore = defineStore('tokenize', () => {
   const tokenLength = ref(0);
 
   async function checkTokens(str: string) {
-    tokenLength.value = 0;
-    try {
-      const { tokens } = await $http.post('/api/tokenize', { stringToTokenize: str });
-
-      tokenLength.value = tokens;
-    } catch (err) {
-      console.error(err);
-    }
+    const encoded = encode(str);
+    tokenLength.value = encoded.length;
   }
 
   return { checkTokens, tokenLength };
