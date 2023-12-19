2e06b09a77235f9b7c6f22828b9ba4510d0c2c6b
David Soards <david@soards.me>
2023-12-18 22:37:59 -0500

    SVG title mobile tweaks

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,17 +8,22 @@ import ChatLogoSVG from '@/components/ChatLogoSVG.vue';
   <header class="bg-gray-900 px-4 py-2 sm:px-6">
     <div class="flex items-center justify-between text-gray-300">
       <ChatLogoSVG class="h-10 p-1.5" />
-      <!-- <RouterLink to="/chat" class="text-sm font-medium hover:text-white">Login</RouterLink> -->
+      <!-- <RouterLink
+        to="/chat"
+        class="rounded px-3 py-1.5 text-sm font-medium transition-colors hover:bg-white/5 hover:text-white"
+      >
+        Login
+      </RouterLink> -->
     </div>
   </header>
   <div class="h-full overflow-hidden bg-radial-glow">
     <div class="grid h-full place-content-center px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
       <div class="max-w-2xl text-center">
         <h2
-          class="mt-8 flex items-center text-4xl font-semibold tracking-tight text-white sm:text-6xl"
+          class="mt-8 flex flex-wrap items-center justify-center gap-4 text-5xl font-semibold tracking-tight text-white sm:text-6xl"
         >
           Move faster with
-          <ChatLogoSVG class="ml-4 inline-block h-12" />
+          <ChatLogoSVG class="inline-block h-9 sm:h-12" />
         </h2>
         <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
           Slowness anywhere justifies slowness everywhere.
@@ -28,7 +33,7 @@ import ChatLogoSVG from '@/components/ChatLogoSVG.vue';
         <div class="mt-10 flex items-center justify-center gap-x-6">
           <RouterLink
             to="/chat"
-            class="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm transition-colors hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+            class="rounded-md bg-white px-4 py-2 text-sm font-semibold text-gray-900 shadow-sm transition-colors hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
           >
             Chat
           </RouterLink>
