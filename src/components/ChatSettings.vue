f151424c7b7491a6f199ceec0c0b15bc0253ba43
David Soards <david@soards.me>
2024-01-06 21:48:22 -0500

    add collapse to settings section

--- src/components/ChatSettings.vue
+++ src/components/ChatSettings.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { DButton, DRange, DSelect, DTextarea } from 'deez-components';
+import { DCollapse, DRange, DSelect, DTextarea } from 'deez-components';
 
 import { MODELS, useChatStore } from '@/stores/chat';
 
@@ -7,46 +7,47 @@ const chatStore = useChatStore();
 </script>
 
 <template>
-  <div class="flex flex-col gap-8 p-4 pb-12">
-    <h2 class="text-xl font-semibold">Settings</h2>
-    <DSelect id="model" v-model="chatStore.model" label="Model">
-      <option value="" disabled>Choose an LLM</option>
-      <option v-for="model in MODELS" :key="model">
-        {{ model }}
-      </option>
-    </DSelect>
+  <DCollapse button-text="Settings">
+    <div class="flex flex-col gap-8 px-4 pb-6 pt-3">
+      <DSelect id="model" v-model="chatStore.model" label="Model">
+        <option value="" disabled>Choose an LLM</option>
+        <option v-for="model in MODELS" :key="model">
+          {{ model }}
+        </option>
+      </DSelect>
 
-    <div>
-      <DRange
-        id="temperature"
-        v-model="chatStore.temperature"
-        label="Temperature"
-        :min="0"
-        :max="2"
-        :step="0.1"
+      <div>
+        <DRange
+          id="temperature"
+          v-model="chatStore.temperature"
+          label="Temperature"
+          :min="0"
+          :max="2"
+          :step="0.1"
+        />
+      </div>
+
+      <div>
+        <DRange
+          id="maxTokens"
+          v-model="chatStore.maxTokens"
+          label="Max Token Length"
+          :min="256"
+          :max="4096"
+          :step="256"
+        />
+      </div>
+
+      <DTextarea
+        id="systemMessage"
+        v-model="chatStore.systemMessage"
+        label="System Message"
+        :rows="6"
+        placeholder="You are a helpful assistant."
       />
+      <!-- <div>
+            <DButton @click="chatStore.$reset">Clear</DButton>
+          </div> -->
     </div>
-
-    <div>
-      <DRange
-        id="maxTokens"
-        v-model="chatStore.maxTokens"
-        label="Max Token Length"
-        :min="256"
-        :max="4096"
-        :step="256"
-      />
-    </div>
-
-    <DTextarea
-      id="systemMessage"
-      v-model="chatStore.systemMessage"
-      label="System Message"
-      :rows="6"
-      placeholder="You are a helpful assistant."
-    />
-    <div>
-      <DButton @click="chatStore.$reset">Clear</DButton>
-    </div>
-  </div>
+  </DCollapse>
 </template>
---
+++ src/components/ChatSidebar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// import { DButton } from 'deez-components';
+
+import ChatSettings from './ChatSettings.vue';
+
+// import IconPenSquare from '~icons/lucide/pen-square';
+</script>
+s
+<template>
+  <div class="grid pb-12">
+    <ChatSettings />
+    <div class="-mx-2.5 flex flex-col items-center px-4 py-3">
+      <!-- <DButton class="w-full justify-between dark:bg-transparent dark:hover:bg-white/5">
+        New Chat
+        <IconPenSquare class="size-4" />
+      </DButton> -->
+    </div>
+  </div>
+</template>
