4786a3dbb3c3af45ee25d4cb7346cd9c1a252afc
David Soards <david@soards.me>
2024-03-02 13:19:17 -0500

    use defineModel for UserMessageInput

--- src/components/UserMessageInput.vue
+++ src/components/UserMessageInput.vue
@@ -1,36 +1,20 @@
 <script setup lang="ts">
-import { computed } from 'vue';
 import { DButton, DTextarea } from 'deez-components';
 
 import IconSend from '~icons/majesticons/send';
 
-const props = withDefaults(
-  defineProps<{
-    modelValue?: string;
-  }>(),
-  { modelValue: '' },
-);
+const model = defineModel<string>();
 
 const emit = defineEmits<{
   send: [];
-  'update:modelValue': [message: string];
 }>();
-
-const message = computed({
-  get() {
-    return props.modelValue;
-  },
-  set(val) {
-    emit('update:modelValue', val);
-  },
-});
 </script>
 
 <template>
   <div class="scrollbar-hide mx-auto w-full max-w-[72ch] overflow-y-scroll">
     <DTextarea
       id="userMessage"
-      v-model="message"
+      v-model="model"
       label="User Message"
       class="absolute inset-0 max-h-52 resize-none pr-10"
       @keydown.enter.exact.prevent="emit('send')"
@@ -39,7 +23,7 @@ const message = computed({
         <div
           class="invisible max-h-52 min-h-[36px] overflow-y-hidden whitespace-pre-wrap px-3 py-1.5 pr-10 sm:text-sm sm:leading-6"
         >
-          {{ props.modelValue }}
+          {{ model }}
         </div>
       </template>
       <template #after>
