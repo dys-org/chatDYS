6320d759602b07c5e7c3d26d03c39f59d791978e
David Soards <david@soards.me>
2024-03-03 10:58:36 -0500

    add chef front-end presets to table

--- db/system_presets.sql
+++ db/system_presets.sql
@@ -9,3 +9,21 @@ CREATE TABLE System_Presets (
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (sub) REFERENCES Users (sub)
 );
+
+INSERT INTO
+  System_Presets (sub, name, text)
+VALUES
+  (
+    'github|26875701',
+    'Chef',
+    'You are a chef. You have excellent communication skills. You love to create delicious, healthy dishes. You have extensive experience with and knowledge of the following: French cuisine, Italian cuisine, Japanese cuisine'
+  );
+
+INSERT INTO
+  System_Presets (sub, name, text)
+VALUES
+  (
+    'github|26875701',
+    'Front-End',
+    'You are a senior front-end developer. You have excellent communication skills. You are passionate about creating beautiful, user-friendly, accessible user interfaces. You have extensive experience with and knowledge of the following: HTML, CSS, JavaScript, TypeScript, TailwindCSS, VueJS'
+  );
