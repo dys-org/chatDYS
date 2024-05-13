a0f88e2dea1863226352fb6384347b5b8520fcea
David Soards <david@soards.me>
2024-05-12 23:27:34 -0400

    find migrations folder

--- packages/server/src/drizzle/migrate.ts
+++ packages/server/src/drizzle/migrate.ts
@@ -3,5 +3,5 @@ import { migrate } from 'drizzle-orm/better-sqlite3/migrator';
 import { db, sqlite } from './db.js';
 
 // This will run migrations on the database, skipping the ones already applied
-migrate(db, { migrationsFolder: 'src/drizzle' });
+migrate(db, { migrationsFolder: 'src/drizzle/migrations' });
 sqlite.close();
