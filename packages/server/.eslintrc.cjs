eb47901555f78e10e27981f42697d685c63308d7
David Soards <david@soards.me>
2024-05-27 11:49:11 -0400

    need to fix type lint rule

--- packages/server/.eslintrc.cjs
+++ packages/server/.eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
-    '@typescript-eslint/consistent-type-imports': 'error',
+    // '@typescript-eslint/consistent-type-imports': 'error',
   },
   ignorePatterns: ['dist'],
 };
